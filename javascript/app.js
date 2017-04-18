3947e564d280e4bacd00941b07b2cecf8b6ebb48
Manish Bisht <manish.bisht490@gmail.com>
2017-04-18 22:51:40 +0530

    flipkart api http request

--- javascript/app.js
+++ javascript/app.js
@@ -7,4 +7,27 @@
 // Immediately Invoked function expression
 (function () {
     var app = angular.module('FlipkartProductSearchApp', ['ngMaterial']);
+    app.controller('searchCtrl', function ($scope, $http){
+        $scope.search = function () {
+            if($scope.query){
+                console.log($scope.query);
+                /*$http.defaults.headers.common['Fk-Affiliate-Id'] = 'bestshopp';
+                $http.defaults.headers.common['Fk-Affiliate-Token'] = '21719d092a4c47bba14ee26dc9726988';
+                $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';*/
+                $http({
+                    method : "GET",
+                    url : "https://affiliate-api.flipkart.net/affiliate/search/json?resultCount=10&query="+$scope.query,
+                    headers: {
+                        'Fk-Affiliate-Id': 'bestshopp',
+                        'Fk-Affiliate-Token': '21719d092a4c47bba14ee26dc9726988',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Headers': 'Origin'
+                    }
+                }).success(function mySucces(response) {
+                    console.log(response.data);
+                });
+            }
+
+        }
+    });
 })();
